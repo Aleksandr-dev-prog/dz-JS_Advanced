b9308413b4614992f8e4a456e27dba1dc0316d27
Aleksandr <aleksandr.pankov.87@list.ru>
2026-01-11 08:05:18 +0300

    1-map (M)

--- 1-map/app.js
+++ 1-map/app.js
@@ -6,10 +6,33 @@ let arrObj = [
     { id: 1, name: "Вася" },
 ];
 
+// 1 способ (не эффективный)
 let unique = Array.from(new Set(arrObj.map(elem => JSON.stringify(elem)))).map(elem => JSON.parse(elem));
-console.log(unique);
+console.log(unique);// Выведет: [ { id: 1, name: "Вася" }, { id: 2, name: "Петя" } ]
 
+// 2 способ - Испольуем Map, где ключом будет id, а значением объект
+const uniqArr = Array.from(
+    new Map(arrObj.map(obj => [obj.id, obj])).values()
+);
+console.log(uniqArr);// Выведет: [ { id: 1, name: "Вася" }, { id: 2, name: "Петя" } ]
 
+// 3 способ
+const uniqArr1 = Object.values(
+  arrObj.reduce((acc, obj) => {
+    acc[obj.id] = obj;
+    return acc;
+  }, {})
+);
+console.log(uniqArr1);// Выведет: [ { id: 1, name: "Вася" }, { id: 2, name: "Петя" } ]
+
+// 4 способ - Если нужно оставить первый встретившийся объект с уникальным id (а не последний)
+const set = new Set();
+const uniqArr2 = arrObj.filter(obj => {
+  if (set.has(obj.id)) return false;
+  set.add(obj.id);
+  return true;
+});
+console.log(uniqArr2);// Выведет: [ { id: 1, name: "Вася" }, { id: 2, name: "Петя" } ]
 
 
 
